b4eacc02aa303f8b6410ce846765c5e7fe3ccd6d
Peter Kropf <pkropf@gmail.com>
2011-01-08 22:33:56 -0800

    now using the Servo object from commands.py.

--- spin.py
+++ spin.py
@@ -1,18 +1,18 @@
 #! /usr/bin/env python
 
-from servo import Servo
+from commands import Servo
 from time import sleep
 
 
 s1 = Servo(9, '/dev/tty.usbmodemfa141')
 s2 = Servo(10, '/dev/tty.usbmodemfa141')
 
-for z in range(10):
+for z in range(3):
     for x in range(180):
-        s1.move(x)
-        s2.move(180-x)
+        s1.send(x)
+        s2.send(180-x)
         sleep(0.02)
 
-    s1.move(0)
-    s2.move(180)
+    s1.send(0)
+    s2.send(180)
     sleep(1)
