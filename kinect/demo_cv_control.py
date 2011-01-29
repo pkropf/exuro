93eb4b3f14004505422ff9bb709a513039e7f4cf
Peter Kropf <pkropf@gmail.com>
2011-01-28 21:30:38 -0800

    added a configuration file instead of having values hardcoded in all the programs. added a LeftEye and RightEye to the eye.py module. added a couple of more work-in-progress demos for interacting with the kinect.

---
+++ kinect/demo_cv_control.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import freenect
+import cv
+import frame_convert
+import numpy as np
+
+
+threshold = 100
+current_depth = 0
+kill_me = False
+
+
+def change_threshold(value):
+    global threshold
+    threshold = value
+
+
+def change_depth(value):
+    global current_depth
+    current_depth = value
+
+
+def depth(dev, data, timestamp):
+    global threshold, current_depth, kill_me
+
+    depth = 255 * np.logical_and(data >= current_depth - threshold,
+                                 data <= current_depth + threshold)
+    depth = depth.astype(np.uint8)
+    image = cv.CreateImageHeader((depth.shape[1], depth.shape[0]),
+                                 cv.IPL_DEPTH_8U,
+                                 1)
+    cv.SetData(image, depth.tostring(),
+               depth.dtype.itemsize * depth.shape[1])
+    cv.ShowImage('Depth', image)
+
+    key = cv.WaitKey(10)
+    if key == 27:
+        kill_me = True
+
+
+def video(dev, data, timestamp):
+    global kill_me
+
+    cv.ShowImage('Video', frame_convert.video_cv(data))
+    key = cv.WaitKey(10)
+    if key == 27:
+        kill_me = True
+
+
+cv.NamedWindow('Depth')
+cv.NamedWindow('Video')
+cv.CreateTrackbar('threshold', 'Depth', threshold,     500,  change_threshold)
+cv.CreateTrackbar('depth',     'Depth', current_depth, 2048, change_depth)
+
+print('Press ESC in window to stop')
+
+
+def body(dev, ctx):
+    global kill_me
+
+    if kill_me:
+        raise freenect.Kill
+
+
+freenect.runloop(depth = depth,
+                 video = video,
+                 body  = body)
---
+++ kinect/demo_cv_reduce.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import freenect
+import cv
+import frame_convert
+import numpy as np
+
+
+threshold = 30
+current_depth = 750
+
+
+def change_threshold(value):
+    global threshold
+    threshold = value
+
+
+def change_depth(value):
+    global current_depth
+    current_depth = value
+
+
+def show_depth():
+    global threshold
+    global current_depth
+
+    depth, timestamp = freenect.sync_get_depth()
+    depth = 255 * np.logical_and(depth >= current_depth - threshold,
+                                 depth <= current_depth + threshold)
+    depth = depth.astype(np.uint8)
+    image = cv.CreateImageHeader((depth.shape[1], depth.shape[0]),
+                                 cv.IPL_DEPTH_8U,
+                                 1)
+    cv.SetData(image, depth.tostring(),
+               depth.dtype.itemsize * depth.shape[1])
+    cv.ShowImage('Depth', image)
+
+    print 'depth:', type(depth), dir(depth)
+    print ''
+
+    print 'image:', type(image), dir(image)
+    print ''
+
+    x = cv.CreateMat(image.height, image.width, cv.IPL_DEPTH_8U)
+    cv.Convert(image, x)
+
+    smaller = cv.CreateMat(image.height / 10, image.width / 10, cv.IPL_DEPTH_8U)
+    print 'smaller:', type(smaller), dir(smaller)
+    print ''
+
+    cv.Resize(image, smaller)
+    cv.ShowImage('Reduced', smaller)
+
+
+def show_video():
+    cv.ShowImage('Video', frame_convert.video_cv(freenect.sync_get_video()[0]))
+
+
+cv.NamedWindow('Depth')
+cv.NamedWindow('Video')
+cv.NamedWindow('Reduced')
+cv.CreateTrackbar('threshold', 'Depth', threshold,     500,  change_threshold)
+cv.CreateTrackbar('depth',     'Depth', current_depth, 2048, change_depth)
+
+print('Press ESC in window to stop')
+
+
+while 1:
+    show_depth()
+    show_video()
+    if cv.WaitKey(5) == 27:
+        break
